38db1d80b896a6361d8dff4848e634ed835a38c0
shaik ameer basha <shaikshabana758@gmail.com>
2022-03-07 16:00:43 +0530

    [shabana]CheckingifEmployeepresentorabsent

---
+++ EmployeeWage.js
@@ -0,0 +1,8 @@
+const IS_ABSENT=0;
+  let empcheck=Math.floor(Math.random()*10) %2;
+  if(empcheck==IS_ABSENT){
+      console.log("UC1-Employee is ABSENT.");
+  } else{
+  console.log("UC1-Employee is PRESENT.");
+  }
+ 
---
+++ shaik/EmployeeWage.js
@@ -0,0 +1,9 @@
+  const IS_ABSENT=0;
+  let empcheck=Math.floor(Math.random()*10) %2;
+  if(empcheck==IS_ABSENT){
+      console.log("UC1-Employee is ABSENT.");
+  } else{
+  console.log("UC1-Employee is PRESENT.");
+  }
+
+
