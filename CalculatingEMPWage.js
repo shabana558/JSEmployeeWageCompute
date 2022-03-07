fa22cb03ea58d9f40dd36e69aecebd60f80f08b4
shaik ameer basha <shaikshabana758@gmail.com>
2022-03-07 19:05:00 +0530

    [shabana]CalculatingWagesofamonth

---
+++ CalculatingEMPWage.js
@@ -0,0 +1,9 @@
+//UC4
+const NUM_OF_WORKING_DAYS =2;
+let empHrs =0;
+for (let day=0;day<NUM_OF_WORKING_DAYS;day++) {
+    let empCheck=Math.floor(Math.random()*10) % 3;
+    empHrs += getWorkingHours(empCheck);
+}
+let empWage=empHrs*WAGE_PER_HOUR;
+console.log("Total Hrs:"+empHrs+"empWage:"+ empWage);
