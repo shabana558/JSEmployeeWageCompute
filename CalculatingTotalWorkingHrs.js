84621b7bee25e215b11914e37e17508957911e93
shaik ameer basha <shaikshabana758@gmail.com>
2022-03-09 11:46:14 +0530

    [shabana]CalculatingtotalworkingHrs

---
+++ CalculatingTotalWorkingHrs.js
@@ -0,0 +1,13 @@
+//UC5
+const MAX_HRS_IN_MONTH =100;
+const NUM_OF_WORKING_DAYS=10;
+let totalEmpHrs=0;
+let totalWorkingDays=0;
+while (totalEmpHrs <= MAX_HRS_IN_MONTH&&
+    totalWorkingDays<NUM_OF_WORKING_DAYS) {
+        totalWorkingDays++;
+        let empCheck=Math.floor(Math.random() *10) % 3;
+        totalEmpHrs += getWorkingHours(empCheck);
+    }
+    let empWage= totalEmpHrs*WAGE_PER_HOUR;
+    console.log("UC5-Total Days:"+ totalWorkingDays +"Total Hrs"+totalEmpHrs+"Emp Wage"+empWage);
